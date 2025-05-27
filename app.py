58caf2e5989adc355801c66e8c1fa1376f787bd2
raiaruna79 <raiaruna899@gmail.com>
2025-05-27 21:18:01 +0545

    second commit

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app=Flask(__name__)
 
 def webout():
 
- return '<h1>DevOps is so much fun to learn now.</h1>'
+ return '<h1>DevOps is so much fun to learn now and more easy to learn now bca.</h1>'
 
 app.run(host='0.0.0.0',port=7000)
